936244e30adc30c57df33b0840d7ab98d45c283c
eric.brewer <eric.brewer@sandboxww.com>
2016-05-06 13:21:00 -0400

    moderate text memory in case you selected the wrong name

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
 (function($){
+  window.assemblaContent = false;
   var handleKeydown = function(e){
+    if(window.assemblaContent !== false){
+      if(e.keyCode == 8){
+        $('#new-comment').val(window.assemblaContent);
+      }
+      window.assemblaContent = false; 
+    }
     if($('#auto_list').css('display') == 'block'){
       if(e.keyCode == 9 ){
         var first = $('#auto_list li').first()[0];
@@ -12,7 +19,9 @@
         })
         if($(first).attr('data-value') != '@members'){
           e.preventDefault();
+          var text = $('#new-comment').val();
           $(first).trigger('click');
+          window.assemblaContent = text;
         }
       }
     }
